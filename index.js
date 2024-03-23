e459e0013dfbd20ea2d434436538818b6f0bb707
unknown <aramdeveloper1@gmail.com>
2024-03-23 15:52:54 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your code here
+const formData = {
+   dogName: "Byron",
+   dogBreed: "Poodle",
+};
+
+const configurationObject = {
+   method: "POST",
+   headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+   },
+   body: JSON.stringify(formData),
+};
+
+fetch("http://localhost:3000/dogs", configurationObject);
